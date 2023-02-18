b5b26a0af9f0097ee39d467549aadf8d803070d8
Raghava Gatadi <raghava.gtd@gmail.com>
2023-02-18 18:59:22 +0530

    changed home.html name to base.html

--- testing/views.py
+++ testing/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.views.generic import ListView,DetailView,TemplateView
 
 class HomePageView(TemplateView):
-    template_name='base.html'
+    template_name='home.html'
