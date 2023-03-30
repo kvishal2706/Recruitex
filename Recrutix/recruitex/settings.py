c0cad4636914d6aa33ee113da753bf0ab8d5512a
Raghava Gatadi <raghava.gtd@gmail.com>
2023-03-30 21:57:57 +0530

    newsletter status:not working

--- Recrutix/recruitex/settings.py
+++ Recrutix/recruitex/settings.py
@@ -32,7 +32,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'raghavagatadi12@gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_PASSWORD = "zxlijkxmwvljxvdm"
+EMAIL_HOST_PASSWORD = "Raghava@07"
 
 
 ALLOWED_HOSTS = []
